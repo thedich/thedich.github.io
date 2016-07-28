a5dcb2e668f58577bc81b34d7dbc05d89c45af74
dich <dchernnykh@ucs.ru>
2016-07-28 18:42:14 +0300

    logo projects

---
+++ self/samsung/paper.js
@@ -0,0 +1,14 @@
+/*
+ * #distinctive
+ */
+
+// Paper.js loader for development, as produced by the build/load.sh script
+document.write('<script type="text/javascript" src="lib/prepro.js"></script>');
+document.write('<script type="text/javascript" src="src/load.js"></script>');
+
+segment = new Point().getDistance()
+//new Path().getPathData()
+//project.scale()
+//new Layer().clear()
+//var hitResult = project.hitTest(cevent.point, hitOptions);
+//hitResult.getLayer()
